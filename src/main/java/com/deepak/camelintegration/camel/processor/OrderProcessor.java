ce7572d0d52623bf699108fec5e900cba6a3305e
Deepak Valluru <deepakvalluru@gmail.com>
2020-04-11 01:19:51 -0800

    Updated Rabbit MQ endpoints.

--- src/main/java/com/deepak/camelintegration/camel/processor/OrderProcessor.java
+++ src/main/java/com/deepak/camelintegration/camel/processor/OrderProcessor.java
@@ -5,13 +5,15 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 //@Component
 public class OrderProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         Order order = (Order) exchange.getIn().getBody();
         System.out.println( order );
-        order.setOrderNumber("UPDATED");
+        order.setOrderNumber("UPDATED" + Instant.now() );
         exchange.getIn().setBody(order);
     }
 }
--- src/main/java/com/deepak/camelintegration/camel/routes/RestOrderToRabbitMQRoute.java
+++ src/main/java/com/deepak/camelintegration/camel/routes/RestOrderToRabbitMQRoute.java
@@ -5,18 +5,19 @@ import com.deepak.camelintegration.model.Order;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestOrderToRabbitMQRoute extends RouteBuilder
 {
-    private static String mqAddress = "rabbitmq://localhost:5672/orders-exchange?" +
+    private static String FROM_RABBIT_MQ = "rabbitmq://localhost:5672/orders-exchange?" +
             "queue=orders-queue-priority&exchangeType=topic&routingKey=orders.priority&" +
             "deadLetterExchange=orders.exchange.dle&deadLetterExchangeType=topic&" +
-            "deadLetterQueue=orders.dlq&deadLetterRoutingKey=#&autoDelete=false";
+            "deadLetterQueue=orders.dlq&deadLetterRoutingKey=orders.priority&autoDelete=false";
 
-    private static String mqAddress1 = "rabbitmq://localhost:5672/orders-exchange?" +
+    private static String TO_RABBIT_MQ = "rabbitmq://localhost:5672/orders-exchange?" +
             "skipQueueDeclare=true&exchangeType=topic&routingKey=orders.priority&autoDelete=false";
 
     @Override
@@ -34,10 +35,15 @@ public class RestOrderToRabbitMQRoute extends RouteBuilder
                 .route()
                 .process( new OrderProcessor() )
                 .marshal( jsonDataFormat )
-                .log("Response: {body}")
-                .to(ExchangePattern.InOnly, mqAddress1)
-                .to("stream:out")
-                .end();
+                .log("Payload sent to RabbitMQ: ${body}")
+                .to(ExchangePattern.InOnly, TO_RABBIT_MQ)
+                .end()
+                .unmarshal()
+                .json(JsonLibrary.Jackson);
+
+        from(FROM_RABBIT_MQ)
+                .id("rabbitMQToLogRoute")
+                .log("Payload from RabbitMQ: ${body}");
 
     }
 }
